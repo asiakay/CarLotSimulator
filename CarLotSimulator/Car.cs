2d91b317824b7d4fbba787646d2446f98d880837
Asia Grady <66960776+asiakay@users.noreply.github.com>
2020-07-15 21:13:46 -0400

    created new instances of the car class. made a carlot class to handle a list of the new instances of the car class

---
+++ CarLotSimulator/Car.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//This is the Car class. We will use this class to create object instances,
+//in this case, New CarObject = Car will have the same members & properties of the car class.
+//any new "Car objects" will have inherit the behavior of the class members.
+
+
+////DONE Create a seperate class file called Car
+////DONE Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+////DONE Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+////The methods should take one string parameter: the respective noise property
+
+
+
+namespace CarLotSimulator
+    //scope inside namespace
+{
+    public class Car
+        //scope inside class Car
+    {
+        // default empty constructor(){}
+        public Car()
+        {
+            //empty scope
+        }
+        //new constructor renames properties inside the method
+        // public Class(datatype xname, datatype xname,){};
+        // Are we turning a property into a parameter?
+        public Car(int year,
+            string make,
+            string model,
+            string enginenoise,
+            string honknoise,
+            bool isdriveable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = enginenoise;
+            HonkNoise = honknoise;
+            IsDriveable = isdriveable;
+        }
+        //setting the properties of the Car Class
+        //? makes the Year property nullable
+        public int? Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+        // The two methods, returning string variables
+        // return type was originally a string named _engineNoise
+        // changed it in the MakeEngineNoise() method in the Car class
+        // made the return type a property within the Method parameter
+
+        public void MakeEngineNoise()
+        {
+            // prints to the console
+            Console.WriteLine(EngineNoise);
+        }
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine(HonkNoise);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+//    public Car()
+//    {
+//    }
+//    //This is the method car.
+//    //is it a member of the car class?
+//    //is it a property of the car class?
+//    // A property is a member that provides a flexible
+//    // mechanism to read, write, or compute the value of a private field.
+//    // Properties can be used as if they are public data members,
+//    // but they are actually special methods called accessors.
+//    // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/properties
+
+
+
+
+//}
+
+
+
+
+////DONE Create a seperate class file called Car
+////DONE Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+//// Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+////The methods should take one string parameter: the respective noise property
+
+
+////public Car(string year,
+////           string make,
+////           string model,
+////           string engineNoise,
+////           string honkNoise,
+////           bool isDriveable)
+////{
+////    Year = year;
+////    Make = make;
+////    Model = model;
+////    EngineNoise = engineNoise;
+////    HonkNoise = honkNoise;
+////    IsDriveable = isDriveable;
+////}
+
+////public string Year { get; set; }
+////public string Make { get; set; }
+////public string Model { get; set; }
+////public string EngineNoise { get; set; }
+////public string HonkNoise { get; set; }
+////public bool IsDriveable { get; set; }
+
+//public void MakeEngineNoise(string EngineNose)
+//{
+//    Console.WriteLine(EngineNoise);
+
+//}
+
+//public void MakeHonkNoise(string honkNoise)
+//{
+//    Console.WriteLine(honkNoise);
+//}
+
+
+//    }
+//}
--- CarLotSimulator/Program.cs
+++ CarLotSimulator/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
 
 namespace CarLotSimulator
 {
@@ -6,28 +9,89 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
-            //TODO
+            //Done
 
             //Create a seperate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property            
 
+            //Done
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
+
+            //Done
+
             //Call each of the methods for each car
 
+            //
+            //Instanciate the Carlot at the beginning of the program
+            //and as you create a car add the car to the list.
+            var myLot = new Carlot();
+
+            //First way to created a new instance,
+            //using . notation
+            //now can access the properties of the
+            //Car class to work with the new instance
+            var carA = new Car();
+            carA.Year = 1967;
+            carA.Make = "Ford";
+            carA.Model = "Mustang";
+            carA.EngineNoise = "vroom";
+            carA.HonkNoise = "beep";
+            carA.IsDriveable = true;
+            // Adding carA to local variable myLot we created for list of Cars
+            // in the Carlot class
+            // references travel from bigger container to smaller unit
+            myLot.Cars.Add(carA);
+            //object initializer syntax
+            // - has scope,
+            // properties are separated by a comma
+            var carB = new Car()
+            {
+                Year=2017,
+                Make="Honda",
+                Model="Civic",
+                EngineNoise="tut",
+                HonkNoise="beep",
+                IsDriveable=false,
+            };
+            //Adding carB
+            myLot.Cars.Add(carB);
+            //Construction Pass
+            //will pass through the new constructor we made in Class.cs
+            var carC = new Car(2020, "Tesla", "Whateveritscalled","wrrrr","beep", true);
+            myLot.Cars.Add(carC);
+            //Calling Methods
+            //Instance Method
+            //Parameters go through parenthesis, Methods go through methods,
+            //Instances of a class go through instances of a class
+            //The method has instructions to print to console 
+            carC.MakeEngineNoise();
+            carB.MakeEngineNoise();
+            carA.MakeEngineNoise();
+            //Using a foreach loop is for when you want to apply some factor to each
+            //item in a list, array, etc??
+            //using the local variable car, i can write to console 
+
+            foreach (var car in myLot.Cars)
+                Console.WriteLine($"{car.Year} {car.Make} {car.Model}");
+
+            //Done
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
+            //Done
             //*************BONUS X 2*************//
-
             //Create a CarLot class
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+
+
         }
     }
 }
